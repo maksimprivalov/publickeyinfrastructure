7f40b3a3139aa679fc4cc86fcd24488e1f1cf27b
maksimprivalov <mprivalovs@gmail.com>
2025-09-02 14:55:56 +0200

    add: template for backend

---
+++ pki-backend/src/test/java/com/app/pki_backend/PkiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.pki_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PkiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
