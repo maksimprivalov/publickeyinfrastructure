2b8cc3f2c9bf32b84b4bd4e573516b4466283260
Roman <rart05@yahoo.com>
2025-09-05 15:42:03 +0200

    feature/core: added interface CertificateAuthorityService and its class implementation

---
+++ pki-backend/src/main/java/com/app/pki_backend/service/implementations/CertificateAuthorityServiceImpl.java
@@ -0,0 +1,7 @@
+package com.app.pki_backend.service.implementations;
+
+import com.app.pki_backend.service.interfaces.CertificateAuthorityService;
+
+public class CertificateAuthorityServiceImpl implements CertificateAuthorityService {
+
+}
