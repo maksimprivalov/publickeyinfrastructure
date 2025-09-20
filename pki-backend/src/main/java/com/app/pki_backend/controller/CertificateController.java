625cfce22983432cf1e4c622878486f8ba248ed7
maksimprivalov <mprivalovs@gmail.com>
2025-09-20 21:49:50 +0200

    WIP: CertificateController changes

---
+++ pki-backend/src/main/java/com/app/pki_backend/controller/CertificateController.java
@@ -0,0 +1,11 @@
+package com.app.pki_backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/controller")
+public class CertificateController {
+
+}
