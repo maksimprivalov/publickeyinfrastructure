044bf0cd2c0228b5c3b69249c03efa14ce16a44b
maksimprivalov <mprivalovs@gmail.com>
2025-09-22 21:18:13 +0200

    feature/certificate-management: fixed bug with dependency

--- pki-backend/src/main/java/com/app/pki_backend/repository/CertificateRepository.java
+++ pki-backend/src/main/java/com/app/pki_backend/repository/CertificateRepository.java
@@ -11,7 +11,8 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
+
 import java.math.BigInteger;
 import java.time.LocalDateTime;
 import java.util.List;
